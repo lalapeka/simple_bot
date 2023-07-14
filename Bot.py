8b7e2060819e94bf783f61a6f92d3af6aac203fb
Roman <morozovrom1@gmail.com>
2023-07-14 16:12:44 +0500

    Фразы добавил и логирование

--- Bot.py
+++ Bot.py
@@ -1,29 +1,47 @@
 import logging
 import settings
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filtres
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                   level=logging.INFO,
                   filename='bot.log'
                   )
 
+
 def start_bot(update: Updater, context: CallbackContext):
     mytext = """Привет, {}
 
-    Давай сыграем в игру =)""".format(update.message.chat.first_name)
+   """.format(update.message.chat.first_name)
+    logging.info('User {} press /start'.format(update.message.chat.username))
+
     update.message.reply_text(mytext)
+    logging.info(mytext)
 
-def chat(update: Updater, context: CallbackContext):
-    text = "Привет"
-    logging.info(text)
+def chat(update, context):
+    text = update.message.text.lower()
 
-    update.message.reply_text(text)    
+    if text == "привет":
+        response = "Привет! Как дела?"
+    elif text == "Нормально":
+        response = "Ну и иди нахуй тогда, чмо"
+    elif text == "Хорошо":
+        response = "Жаль, думал ты помер"
+    elif text == "Почему?":
+        response = "А это тебя ебать не должно, ок?"
+    elif text == "ок":
+        response = "Ну вот и порешили"
+    else:
+        response = "Заебал блять, пиши сука нормально уебан"
+
+    logging.info("User {} sent message: {}".format(update.message.chat.username, text))
+    logging.info("Bot responded with message: {}".format(response))
+    
+    update.message.reply_text(response)    
 
 def main():
     updr = Updater(settings.TOKEN_TELEGRAMM)
     updr.dispatcher.add_handler(CommandHandler("start", start_bot))
-        updr.dispatcher.add_handler(MessageHandler(Filtres.text, chat))
-
+    updr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
 
     updr.start_polling()
     updr.idle()
